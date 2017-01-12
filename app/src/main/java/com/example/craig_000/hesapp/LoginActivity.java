719861ac7cb8a1c06124bac9e259c7585ba50de9
Craig <craigodawg19@gmail.com>
2017-01-12 18:21:42 -0500

    Sign up implemented and it looks and works prettier now

--- app/src/main/java/com/example/craig_000/hesapp/LoginActivity.java
+++ app/src/main/java/com/example/craig_000/hesapp/LoginActivity.java
@@ -120,7 +120,7 @@ public class LoginActivity extends AppCompatActivity {
                 Log.d("LOGIN", "signInWithEmail:onComplete:" + task.isSuccessful());
                 // If sign in fails, display a message to the user. If sign in succeeds
                 // the auth state listener will be notified and logic to handle the
-                // signed in user can be handled in the listener.
+                // signed in user can be handled in the listener
                 if (!task.isSuccessful()) {
                     signInDialog.dismiss();
                     Log.w("LOGIN", "signInWithEmail:failed", task.getException());
