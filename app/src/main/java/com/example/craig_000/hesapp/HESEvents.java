790b75eb33442253af6cc52972c987f73bb95d31
booneallyson <booneallyson@gmail.com>
2017-03-07 10:26:01 -0500

    added dependency for Caldroid to build.gradle file

--- app/src/main/java/com/example/craig_000/hesapp/HESEvents.java
+++ app/src/main/java/com/example/craig_000/hesapp/HESEvents.java
@@ -1,6 +1,8 @@
 package com.example.craig_000.hesapp;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -12,8 +14,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.roomorama.caldroid.CaldroidFragment;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Map;
 
 /**
@@ -26,18 +31,37 @@ public class HESEvents extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hesevents);
 
+        ListView list = (ListView)findViewById(R.id.myList);
 
         final ArrayList<Event> events = new ArrayList<>();
-        
+
+        final EventAdapter adapter = new EventAdapter(this, events);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference hesEventsRef = database.getReference("hes_events");
+
+        hesEventsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("Firebase HES Events", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Firebase HES Events", "Failed to read value.", error.toException());
+            }
+        });
 
         hesEventsRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 Map<String, Object> map = (Map<String, Object>)dataSnapshot.getValue();
                 events.add(new Event((String)map.get("date"), (String)map.get("time"), (String)map.get("title")));
+                Log.d("Firebase HES Events", "Value is: " + map);
             }
 
             @Override
@@ -62,9 +86,7 @@ public class HESEvents extends AppCompatActivity{
         });
 
 
-        final EventAdapter adapter = new EventAdapter(this, events);
 
-        ListView list = (ListView)findViewById(R.id.myList);
         list.setAdapter(adapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
@@ -75,7 +97,16 @@ public class HESEvents extends AppCompatActivity{
             }
         });
 
+        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        Bundle args = new Bundle();
+        Calendar cal = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        caldroidFragment.setArguments(args);
 
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.replace(R.id.calendar1, caldroidFragment);
+        t.commit();
 
     }
 }
